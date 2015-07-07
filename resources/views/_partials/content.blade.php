a8184ad2424fe7dd920a5af442cebb95646f2b1e
Carsten <carsten@cogents.io>
2015-07-07 16:34:06 -0500

    dev 7

---
+++ resources/views/_partials/content.blade.php
@@ -0,0 +1,19 @@
+@include('flash::message')
+
+@if (count($errors) > 0)
+	@include('_partials.errors')
+@endif
+
+@if (session('status'))
+	<div class="alert alert-success">
+		{{ session('status') }}
+	</div>
+@endif
+
+@yield('content')
+
+{{-- var_dump($errors) --}}
+
+
+{{ trans('kotoba::general.home') }}
+
