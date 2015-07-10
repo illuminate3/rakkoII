1190bd88627d95d76b9594fd86d52d3c4fc97093
illuminate3 <carsten.sapporo@gmail.com>
2015-07-09 20:54:55 -0500

    dev 13.2

---
+++ config/kantoku.php
@@ -0,0 +1,17 @@
+<?php
+
+// php artisan vendor:publish --provider="App\Modules\Kantoku\Providers\KantokuServiceProvider" --tag="config"
+
+
+return [
+
+/*
+|--------------------------------------------------------------------------
+| db settings
+|--------------------------------------------------------------------------
+*/
+// 'kantoku_db' => array(
+// 	'prefix'					=> '',
+// ),
+
+];
