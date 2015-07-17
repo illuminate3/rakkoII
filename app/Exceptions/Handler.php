10364a99df6b4c40edf5ecd56f2332792abd088e
illuminate3 <carsten.sapporo@gmail.com>
2015-07-16 21:14:16 -0500

    tokenmismatch

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -6,8 +6,12 @@ use Exception;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
+use Flash;
+
+
 class Handler extends ExceptionHandler
 {
+
     /**
      * A list of the exception types that should not be reported.
      *
@@ -16,6 +20,7 @@ class Handler extends ExceptionHandler
     protected $dontReport = [
         HttpException::class,
     ];
+
 
     /**
      * Report or log an exception.
@@ -30,6 +35,7 @@ class Handler extends ExceptionHandler
         return parent::report($e);
     }
 
+
     /**
      * Render an exception into an HTTP response.
      *
@@ -39,6 +45,17 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
+
+		if ($e instanceof TokenMismatchException) {
+			//Redirect to login form if session expires
+			Flash::success( trans('kotoba::auth.error.time_out') );
+			return redirect($request->fullUrl());
+//             return redirect($request->fullUrl())->with('errors',
+//                 ["The login form has expired, please try again. In the future, reload the login page if it has been open for several hours."]);
+		}
+
         return parent::render($request, $e);
     }
+
+
 }
