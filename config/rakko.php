5bd64ac9dc25bb0210116802b256e48457163102
Carsten <carsten@cogents.io>
2015-07-21 15:12:45 -0500

    dev 17.2

---
+++ config/rakko.php
@@ -0,0 +1,12 @@
+<?php
+
+// php artisan vendor:publish --provider="App\Modules\Installer\Providers\InstallerServiceProvider" --tag="config"
+
+
+return [
+
+	'installed' => true,
+	'version' => '1.0.0',
+	'installed_date' => ''
+
+];
