08dcd718f8191be6c3596f1bded2780640d8939f
illuminate3 <carsten.sapporo@gmail.com>
2015-09-23 16:22:42 -0500

    dev 15.5.13

--- app/Modules/git_fetch.sh
+++ app/Modules/git_fetch.sh
@@ -2,14 +2,32 @@
 
 clear
 
+echo "----------------------------------------------------"
 echo "git fetch for all repos"
+echo "----------------------------------------------------"
 
 path=~/Sites/laravel/app/Modules/
 
-for repo in Campus Core FileX Gakko Himawari Jinji Kagi Kantoku Menus NewsDesk Origami Profiles Setup; do
-	(cd "${repo}" && git fetch --all && git reset --hard origin/master)
-	echo "${repo}"
-	cd $path
+for repo in Campus Core FileX Gakko Himawari Jinji Kagi Kantoku Menus NewsDesk Origami Profiles Setup
+do
+
+	cd ${repo}
+
+	git fetch --all
+	git reset --hard origin/master
+
+	cd ..
+
+#	dir=`echo "$repo" | tr '[:upper:]' '[:lower:]'`
+#	echo $dir
+#	git remote add origin https://github.com/illuminate3/$dir.git
+#	cd $path
+
+#	(cd "${repo}" && git fetch --all && git reset --hard origin/master)
+#	echo "${repo}"
+#	cd $path
 done
 
+echo "----------------------------------------------------"
 echo "done"
+echo "----------------------------------------------------"
