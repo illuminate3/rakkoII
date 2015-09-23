a0c2559edaeb365fc46b760551ad45c1ae000947
illuminate3 <carsten.sapporo@gmail.com>
2015-09-23 14:07:00 -0500

    dev 15.5.9

--- app/Modules/git_init.sh
+++ app/Modules/git_init.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
+clear
+
+echo "git init and git add origin for GitHub repos"
+
 for repo in Core FileX Himawari Kagi Kantoku Menus NewsDesk Origami Profiles; do
 	(cd "${repo}" && git init && git remote add origin https://github.com/illuminate3/"${repo}".git)
+	echo "${repo}"
 	cd ../
 done
 
+echo "git init and git add origin for Gitlab repos"
 
 for repo in Campus FileX Gakko Setup; do
 	(cd "${repo}" && git init && git remote add origin git@dev.cogents.io:crichter/"${repo}".git)
+	echo "${repo}"
 	cd ../
 done
 
-
-
-
+echo "done"
--- app/Modules/mkdir_repos.sh
+++ app/Modules/mkdir_repos.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+clear
+
+echo "mkdir all repos"
+
 for repo in Campus Core FileX Gakko Himawari Jinji Kagi Kantoku Menus NewsDesk Origami Profiles Setup; do
 	mkdir "${repo}"
+	echo "${repo}"
 done
+
+echo "done"
