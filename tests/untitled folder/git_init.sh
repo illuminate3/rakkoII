e418c77abbfc10f8aecc786c5a979c7c0d9e5187
illuminate3 <carsten.sapporo@gmail.com>
2015-09-23 16:06:00 -0500

    dev 15.5.10

--- tests/untitled folder/git_init.sh
+++ tests/untitled folder/git_init.sh
@@ -6,36 +6,20 @@ echo "----------------------------------------------------"
 echo "git init and git add origin for GitHub repos"
 echo "----------------------------------------------------"
 
-for repo in Core FileX Himawari Kagi Kantoku Menus NewsDesk Origami Profiles
-do
-
-	cd ${repo}
-
-	git init
-	dir=`echo "$repo" | tr '[:upper:]' '[:lower:]'`
-	echo $dir
-	git remote add origin https://github.com/illuminate3/$dir.git)
-
+for repo in Core FileX Himawari Kagi Kantoku Menus NewsDesk Origami Profiles; do
+	(cd ${repo} && git init && git remote add origin https://github.com/illuminate3/${repo}.git)
+	echo ${repo}
 	cd ~/Sites/laravel/app/Modules/
-
 done
 
 echo "----------------------------------------------------"
 echo "git init and git add origin for Gitlab repos"
 echo "----------------------------------------------------"
 
-for repo in Campus Gakko Setup
-do
-
-	cd ${repo}
-
-	git init
-	dir=`echo "$repo" | tr '[:upper:]' '[:lower:]'`
-	echo $dir
-	git remote add origin git@dev.cogents.io:crichter/$dir.git
-
+for repo in Campus Gakko Setup; do
+	(cd ${repo} && git init && git remote add origin git@dev.cogents.io:crichter/${repo}.git)
+	echo ${repo}
 	cd ~/Sites/laravel/app/Modules/
-
 done
 
 echo "----------------------------------------------------"
