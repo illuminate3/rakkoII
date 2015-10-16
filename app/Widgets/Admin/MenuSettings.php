36f81853474ad6fbff6669e75b0401b0017dc73c
illuminate3 <carsten.sapporo@gmail.com>
2015-10-15 22:04:10 -0500

    dev 15.8.5

--- app/Widgets/Admin/MenuSettings.php
+++ app/Widgets/Admin/MenuSettings.php
@@ -29,8 +29,8 @@ class MenuSettings extends Widget
 			{
 
 			$main_menu_id = LMenu::where('name', '=', 'settings')->pluck('id');
-			return Menulink::where('menu_id', '=', $main_menu_id)->orderBy('position')->get();
-//			return Menulink::where('menu_id', '=', $main_menu_id)->IsEnabled()->orderBy('position')->get();
+//			return Menulink::where('menu_id', '=', $main_menu_id)->orderBy('position')->get();
+			return Menulink::where('menu_id', '=', $main_menu_id)->IsEnabled()->orderBy('position')->get();
 
 			},
 			function($children, $item)
