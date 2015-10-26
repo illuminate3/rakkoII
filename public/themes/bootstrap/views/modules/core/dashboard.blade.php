18a38e419f5f184ea042e3f66a8a3a518321ea11
illuminate3 <carsten.sapporo@gmail.com>
2015-10-26 07:35:32 -0500

    dev 16.1.1.1

--- public/themes/bootstrap/views/modules/core/dashboard.blade.php
+++ public/themes/bootstrap/views/modules/core/dashboard.blade.php
@@ -44,6 +44,7 @@
 		<hr>
 	</h2>
 
+		<div class="row">
 		<div class="col-sm-6">
 
 			<div class="panel panel-info">
@@ -84,6 +85,46 @@
 				</div>
 			</div>
 
+		</div>
+		<div class="col-sm-6">
+		</div>
+		</div>
+
+	@endif
+
+	@if (Auth::user()->can('manage_shisan'))
+
+	<h2>
+		<i class="fa fa-keyboard-o fa-lg"></i>
+		{{ Lang::choice('kotoba::shop.asset', 2) }}
+		<hr>
+	</h2>
+
+		<div class="row">
+		<div class="col-sm-6">
+
+			<dl class="dl-horizontal">
+				<dt>
+					{{ trans('kotoba::general.all') }}
+				</dt>
+				<dd>
+					<a href="{{ URL::to('/admin/asset') }}">{{ $total_assets }}</a>
+				</dd>
+			</dl>
+
+		</div>
+		<div class="col-sm-6">
+
+			<dl class="dl-horizontal">
+				<dt>
+					{{ date("Y") }}&nbsp;{{ Lang::choice('kotoba::general.year', 1) }}
+				</dt>
+				<dd>
+					<a href="{{ URL::to('/admin/asset') }}">{{ $total_assets_year }}</a>
+				</dd>
+			</dl>
+
+		</div>
 		</div>
 
 	@endif
