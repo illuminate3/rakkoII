8f3c716781e410c6663edfa0ceac829c8135a1fa
illuminate3 <carsten.sapporo@gmail.com>
2016-04-05 15:55:51 -0500

    dev 16.5.0

---
+++ public/themes/bsd/views/_layouts/app.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<meta name="author" content="{{ Setting::get('author', Config::get('core.author')) }}" />
+	<meta name="keywords" content="{{ Setting::get('keywords', Config::get('core.keywords')) }}" />
+	<meta name="description" content="{{ Setting::get('description', Config::get('core.description')) }}" />
+
+	<title>
+		@section('title')
+			{{ Setting::get('title', Config::get('core.title')) }}
+		@show
+	</title>
+
+	<link rel="shortcut icon" href="{{ asset('ico/favicon.png') }}">
+	<link rel="icon" href="{{ asset('favicon.ico') }}">
+	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="{{ asset('ico/apple-touch-icon-57-precomposed.png') }}">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ asset('ico/apple-touch-icon-72-precomposed.png') }}">
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ asset('ico/apple-touch-icon-144-precomposed.png') }}">
+
+<!-- ------------------------------------------ Google Fonts ------------------------------------------ -->
+<!--
+	<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,700' rel='stylesheet' type='text/css'>
+-->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,700' rel='stylesheet' type='text/css'>
+
+<!-- ------------------------------------------ CSS stylesheets ------------------------------------------ -->
+
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/vendors/bootstrap-3.3.5-dist/css/bootstrap.min.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/vendors/font-awesome-4.4.0/css/font-awesome.css') }}">
+
+<!--
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/vendors/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css') }}">
+-->
+
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/vendors/illuminate3/css/standard.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('themes/' . $activeTheme . '/assets/css/style.css') }}">
+
+	<link rel="stylesheet" type="text/css" href="{{ asset('themes/' . $activeTheme . '/assets/mega/css/style.css') }}">
+
+<!-- ------------------------------------------ app loaded CSS stylesheets ------------------------------------------ -->
+	@yield('styles')
+
+<!-- ------------------------------------------ head loaded js ------------------------------------------ -->
+	<script type="text/javascript" src="{{ asset('assets/vendors/jquery/modernizr-2.6.2-respond-1.1.0.min.js') }}"></script>
+
+</head>
+
+<body>
+
+
+<header>
+	@include($activeTheme . '::' . '_partials.mega.navigation')
+</header>
+
+<main>
+{{--
+<main class="cd-main-content">
+--}}
+	@include($activeTheme . '::' . '_partials.content')
+</main>
+
+<footer>
+	@include($activeTheme . '::' . '_partials.footer')
+</footer>
+
+
+<!-- ------------------------------------------ js ------------------------------------------ -->
+
+	<script type="text/javascript" src="{{ asset('assets/vendors/jquery/jquery-2.1.3.min.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('assets/vendors/bootstrap-3.3.5-dist/js/bootstrap.min.js') }}"></script>
+
+	<script type="text/javascript" src="{{ asset('themes/' . $activeTheme . '/assets/mega/js/main.js') }}"></script>
+
+<!--
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.2/js/bootstrap.min.js"></script>
+
+	<script type="text/javascript" src="{{ asset('assets/js/app.js') }}"></script>
+-->
+
+
+<!-- ------------------------------------------ app loaded js ------------------------------------------ -->
+	@yield('scripts')
+
+<!-- ------------------------------------------ template loaded js ------------------------------------------ -->
+	<script type="text/javascript">
+		@yield('inline-scripts')
+	</script>
+
+<!-- ------------------------------------------ google ananlytics js ------------------------------------------ -->
+<script>
+	{!! Setting::get('google_analytics') !!}
+</script>
+<!-- ------------------------------------------ google ananlytics js ------------------------------------------ -->
+
+</body>
+</html>
---
+++ public/themes/bsd/views/_partials/mega/navigation.blade.php
@@ -0,0 +1,315 @@
+<header class="cd-main-header">
+
+	<a class="cd-logo" href="#0"><img src="{{ asset('themes/' . $activeTheme . '/assets/mega/img/brysd.png') }}" alt="Logo"></a>
+
+	<ul class="cd-header-buttons">
+		<li>
+			<a class="cd-search-trigger" href="#cd-search">Search<span></span></a>
+		</li>
+		<li>
+			<a class="cd-nav-trigger" href="#cd-primary-nav">Menu<span></span></a>
+		</li>
+	</ul> <!-- cd-header-buttons -->
+
+</header>
+
+<div class="cd-overlay"></div>
+
+<nav class="cd-nav">
+	<ul id="cd-primary-nav" class="cd-primary-nav is-fixed">
+		<li class="has-children">
+			<a href="http://www.bryantschools.org/about-us">About Us</a>
+
+			<ul class="cd-secondary-nav is-hidden">
+				<li class="go-back"><a href="#0">Go Back</a></li>
+				<li class="see-all"><a href="http://www.bryantschools.org/about-us">Creating Opportunities for Success</a></li>
+				<li class="has-children">
+					<a href="#">Bryant School District</a>
+						<img class="img-responsive" src="img/logo.png">
+					<br />
+					<p>The Bryant School District encompasses 342.19 square miles of land in Saline County and supports all or portions of Bryant, Alexander, Benton, Salem, Mabelvale, Shannon Hills, Paron, Bauxite, and Avilla.</p>
+				</li>
+
+				<li class="has-children">
+					<a href="/school-board-of-education">School Board of Education</a>
+
+					<ul class="is-hidden">
+					<li class="go-back"><a href="#0">Go Back</a></li>
+						<li><a href="http://www.bryantschools.org/board-of-education">Board Members</a></li>
+						<li><a href="http://www.bryantschools.org/themes/district/assets/files/district/files/2015-16%20School%20Board%20Meeting%20Dates.pdf">Meeting Dates &amp; Location</a></li>
+						<li><a href="http://www.bryantschools.org/system/files/documents/000/000/027/2015-16%20School%20Board%20Work%20Sessions.pdf">Work Sessions Dates &amp; Locations</a></li>
+						<li><a href="https://eboard.eboardsolutions.com/Index.aspx?S=36030000">eBoardSolutions</a></li>
+						<li><a href="https://sites.google.com/a/bryantschools.org/board-agenda-and-minutes/home">Board Agenda and Minutes</a></li>
+						<li><a href="http://www.bryantschools.org/themes/district/assets/files/district/files/Board_Policies_2015-2016.pdf">Board Policies</a></li>
+						<li><a href="http://www.bryantschools.org/system/files/documents/000/000/026/board%202015-16%20years%20of%20service.pdf">Board Years of Service</a></li>
+						<li><a href="http://www.bryantschools.org/themes/district/assets/files/district/files/BryantSchoolBoardElection-Zones.pdf">Election Zones</a></li>
+						<li><a href="http://www.bryantschools.org/state-required-information">State Required Information</a></li>
+					</ul>
+				</li>
+
+				<li class="has-children">
+					<a href="http://www.bryantschools.org/themes/district/assets/files/district/files/At_A_Glance.pdf">Our Mission</a>
+					<p>Bryant Public Schools create opportunities for academic and personal success to ensure all students are future ready.</p>
+					<br />
+					<a href="http://www.bryantschools.org/themes/district/assets/files/district/files/At_A_Glance.pdf">Our Vision</a>
+					<p>Bryant Public Schools will be the state's premier provider of educational services.</p>
+				</li>
+
+				<li class="has-children">
+					<a href="http://www.bryantschools.org/communications">Get Connected</a>
+
+					<ul class="is-hidden">
+					<p>We are committed to making sure that information is always available to everyone. Use our website, social media, and messaging systems to get the most up to date information about events happening within the Bryant School District.</p><br />
+					<a href="http://www.facebook.com/bryantschools" style="display: inline;"><i class="fa fa-facebook-official fa-3x"></i></a><a href="http://twitter.com/bryantschools" style="display: inline;"><i class="fa fa-twitter fa-3x"></i></a><a href="http://instagram.com/bryantschools" style="display: inline;"><i class="fa fa-instagram fa-3x"></i></a><a href="https://www.youtube.com/channel/UCfP2_2wyERAesqG6jleZVeA/featured" style="display: inline;"><i class="fa fa-youtube-play fa-3x"></i></a>
+					</ul>
+				</li>
+			</ul>
+		</li>
+
+		<li class="has-children">
+			<a href="http://codyhouse.co/?p=409">Departments</a>
+			<ul class="cd-nav-icons is-hidden">
+				<li class="go-back"><a href="#0">Go Back</a></li>
+				<li class="see-all"><a href="http://www.bryantschools.org/departments">Creating Opportunities for Success</a></li>
+				<li>
+					<a class="cd-nav-item item-1" href="http://www.bryantathletics.com">
+						<h3>Athletics</h3>
+					</a>
+				</li>
+
+				<li>
+					<a class="cd-nav-item item-2" href="https://sites.google.com/a/bryantschools.org/finance-accounting/home">
+						<h3>Business Office</h3>
+					</a>
+				</li>
+
+				<li>
+					<a class="cd-nav-item item-3" href="http://www.bryantschools.org/communications">
+						<h3>Communications</h3>
+					</a>
+				</li>
+
+				<li>
+					<a class="cd-nav-item item-4" href="http://curriculum.bryantschools.org">
+						<h3>Curriculum &amp; Instruction</h3>
+					</a>
+				</li>
+
+				<li>
+					<a class="cd-nav-item item-5" href="http://www.bryantschools.org/early-childhood-education">
+						<h3>Early Childhood Education</h3>
+					</a>
+				</li>
+
+				<li>
+					<a class="cd-nav-item item-6" href="http://www.bryantschools.org/esl">
+						<h3>ESL</h3>
+					</a>
+				</li>
+
+				<li>
+					<a class="cd-nav-item item-7" href="http://www.bryantschools.org/food-service">
+						<h3>Food Service</h3>
+					</a>
+				</li>
+
+				<li>
+					<a class="cd-nav-item item-8" href="http://gt.bryantschools.org">
+						<h3>Gifted Program</h3>
+					</a>
+				</li>
+				<li>
+					<a class="cd-nav-item item-9" href="https://sites.google.com/a/bryantschools.org/finance-accounting/home">
+						<h3>Human Resources</h3>
+					</a>
+				</li>
+				<li>
+					<a class="cd-nav-item item-10" href="http://www.bryantschools.org/nursing">
+						<h3>Nursing</h3>
+					</a>
+				</li>
+				<li>
+					<a class="cd-nav-item item-11" href="https://sites.google.com/a/bryantschools.org/the-parent-center/">
+						<h3>Parent Center</h3>
+					</a>
+				</li>
+				<li>
+					<a class="cd-nav-item item-12" href="http://development.bryantschools.org">
+						<h3>Professional Development</h3>
+					</a>
+				</li>
+				<li>
+					<a class="cd-nav-item item-13" href="http://www.bryantschools.org/school-based-health-center">
+						<h3>School Based Health Center</h3>
+					</a>
+				</li>
+				<li>
+					<a class="cd-nav-item item-14" href="https://sites.google.com/a/bryantschools.org/special-education/">
+						<h3>Special Education</h3>
+					</a>
+				</li>
+				<li>
+					<a class="cd-nav-item item-15" href="http://tech.bryantschools.org">
+						<h3>Technology</h3>
+					</a>
+				</li>
+				<li>
+					<a class="cd-nav-item item-16" href="https://sites.google.com/a/bryantschools.org/transportation/">
+						<h3>Transportation</h3>
+					</a>
+				</li>
+			</ul>
+		</li>
+
+		<li class="has-children">
+			<a href="http://www.bryantschools.org/parents">Parents</a>
+
+			<ul class="cd-secondary-nav is-hidden">
+				<li class="go-back"><a href="#0">Go Back</a></li>
+				<li class="see-all"><a href="http://www.bryantschools.org/parents">Creating Opportunities for Success</a></li>
+				<li class="has-children">
+					<a href="#">Parents</a>
+					<p>Information for the Parents of our students at the Bryant School District!</p>
+
+					<br />
+					<a href="/parents">Resources</a>
+
+					<ul class="is-hidden">
+					<li class="go-back"><a href="#0">Go Back</a></li>
+						<li><a href="http://www.bryantschools.org/themes/district/assets/files/district/files/School_Choice_Application.pdf">School Choice Application</a></li>
+						<li><a href="http://www.arkansased.gov/divisions/learning-services/home-schools">Home School Application</a></li>
+						<li><a href="https://www.google.com/calendar/embed?src=bryantschools.org_pv6ete98g4ig04uo56b9u4ocfg%40group.calendar.google.com&ctz=America/Chicago">Testing Calendar</a></li>
+						<li><a href="https://sites.google.com/a/bryantschools.org/dawson-hippy-program/home">Early Education (HIPPY)</a></li>
+						<li><a href="https://sites.google.com/a/bryantschools.org/special-education/">Special Education</a></li>
+						<li><a href="http://gt.bryantschools.org/">Gifted and Talented</a></li>
+					</ul>
+				</li>
+
+				<li class="has-children">
+					<a href="http://www.bryantschools.org/parents">Information</a>
+
+					<ul class="is-hidden">
+					<li class="go-back"><a href="#0">Go Back</a></li>
+						<li><a href="http://www.bryantschools.org/lunch-menu">Lunch Menus</a></li>
+						<li><a href="https://www.ezschoolpay.com/Login.aspx">EZ School Pay</a></li>
+						<li><a href="http://www.bryantschools.org/district-calendars">District Calendar</a></li>
+						<li><a href="https://docs.google.com/document/d/1U6LwQrRsmK7bFD1LEVusQgRI8HKRzEjjyiT8NQvyneg/pub">Saturday School Guidelines</a></li>
+						<li><a href="http://www.bryantschools.org/school-registration">New Student Registration</a></li>
+						<li><a href="http://www.bryantschools.org/zoning-information">Attendance Zones</a></li>
+						<li><a href="http://www.bryantschools.org/campus-directions">Directions</a></li>
+						<li><a href="https://sites.google.com/a/bryantschools.org/transportation/home/bus-routes">Bus Routes</a></li>
+					</ul>
+				</li>
+
+				<li class="has-children">
+					<a href="/parents">Classroom Help</a>
+
+					<ul class="is-hidden">
+					<li class="go-back"><a href="#0">Go Back</a></li>
+						<li><a href="https://sites.google.com/a/bryantschools.org/the-parent-center/">Parent Center</a></li>
+						<li><a href="http://commoncore.bryantschools.org/">Curriculum Standards</a></li>
+						<li><a href="http://reportcard.bryantschools.org/">Standards Based Report Cards</a></li>
+						<li><a href="http://cc.bryantschools.org/">Concurrent Credit</a></li>
+						<li><a href="http://gt.bryantschools.org/index.php/pre-ap/">Pre-AP/AP</a></li>
+					</ul>
+					<br />
+					<a href="/parents">Handooks</a>
+					<ul class="is-hidden">
+					<li class="go-back"><a href="#0">Go Back</a></li>
+						<li><a href="http://www.bryantschools.org/themes/district/assets/files/district/files/2015-16_Elementary_Student_Handbook.pdf">Elementary</a></li>
+						<li><a href="http://www.bryantschools.org/themes/district/assets/files/district/files/2015-16_Secondary_Student_Handbook.pdf">Secondary</a></li>
+					</ul>
+				</li>
+
+				<li class="has-children">
+					<a href="/parents">Communications</a>
+
+					<ul class="is-hidden">
+					<li class="go-back"><a href="#0">Go Back</a></li>
+						<li><a href="https://hac24.eschoolplus.k12.ar.us/homeaccess24/">HAC (Home Access Center)</a></li>
+						<li><a href="https://bryantschools.bbcportal.com/">Blackboard MyConnect</a></li>
+						<li><a href="http://boardconnect.bryantschools.org/">BoardConnect</a></li>
+						<li><a href="http://www.bryantschools.org/change-of-address">Change of Address</a></li>
+						<li><a href="http://www.bryantschools.org/inclement-weather">Inclement Weather</a></li>
+						<li><a href="http://www.bryantschools.org/community-announcements">Community Announcements</a></li>
+					</ul>
+
+				</li>
+				</li>
+			</ul>
+		</li>
+
+		<li class="has-children">
+			<a href="http://codyhouse.co/?p=409">Students</a>
+
+			<ul class="cd-secondary-nav is-hidden">
+				<li class="go-back"><a href="#0">Go Back</a></li>
+				<li class="see-all"><a href="http://www.bryantschools.org/students">Creating Opportunities for Success</a></li>
+				<li class="has-children">
+					<a href="#">Students</a>
+					<p>Useful information for our students in the Bryant School District!</p>
+<br>
+					<a href="/parents">Handbooks</a>
+
+					<ul class="is-hidden">
+					<li class="go-back"><a href="#0">Go Back</a></li>
+						<li><a href="http://www.bryantschools.org/themes/district/assets/files/district/files/2015-16_Elementary_Student_Handbook.pdf">Elementary</a></li>
+						<li><a href="http://www.bryantschools.org/themes/district/assets/files/district/files/2015-16_Secondary_Student_Handbook.pdf">Secondary</a></li>
+					</ul>
+					<br />
+				</li>
+
+				<li class="has-children">
+					<a href="/parents">Resources</a>
+					<ul class="is-hidden">
+					<li class="go-back"><a href="#0">Go Back</a></li>
+						<li><a href="http://mail.bryantschools.org/">Student Email Access</a></li>
+						<li><a href="http://hac24.eschoolplus.k12.ar.us/homeaccess24/">Home Access Center (HAC)</a></li>
+						<li><a href="http://connection.naviance.com/bryanths">Naviance (Bryant High School Only)</a></li>
+						<li><a href="https://hosted315.renlearn.com/214231/">Renaissance Place</a></li>
+						<li><a href="https://login.i-ready.com/">iReady</a></li>
+						<li><a href="http://www.bryantschools.org/lunch-menu">Lunch Menus</a></li>
+						<li><a href="http://sites.google.com/a/bryantschools.org/transportation/home/bus-routes">Bus Routes</a></li>
+					</ul>
+				</li>
+
+				<li class="has-children">
+
+					<a href="/parents">Information</a>
+					<ul class="is-hidden">
+					<li class="go-back"><a href="#0">Go Back</a></li>
+						<li><a href="http://www.bryantschools.org/school-libraries">School Libraries</a></li>
+						<li><a href="http://www.bryantschools.org/counselors">Counseling Center</a></li>
+						<li><a href="http://www.arkansasscholars.org/scholars_criteria.php">Arkansas Scholar</a></li>
+						<li><a href="http://www.bryantschools.org/athletics-calendar">Athletic Calendar</a></li>
+						<li><a href="http://www.google.com/calendar/embed?src=bryantschools.org_pv6ete98g4ig04uo56b9u4ocfg%40group.calendar.google.com&ctz=America/Chicago">Testing Calendar</a></li>
+						<li><a href="https://pm.bryantschools.org/pm">Password Management</a></li>
+						<li><a href="http://destiny.bryantschools.org/district/servlet/presentlistsitesform.do;jsessionid=6299A53AE36015ECD45BB2310974F6C4?districtMode=true&searchTerm=null">Destiny Online Card Catalog</a></li>
+					</ul>
+				</li>
+
+				<li class="has-children">
+					<a href="/parents">Extracurricular</a>
+					<ul class="is-hidden">
+					<li class="go-back"><a href="#0">Go Back</a></li>
+						<li><a href="http://www.bryantschools.org/cheerdance-tryouts">Cheer/Dance Tryout Information</a></li>
+						<li><a href="http://www.bryantathletics.com/">Athletics</a></li>
+						<li><a href="http://www.thelegacyofbryant.com/">The Legacy of Bryant Band</a></li>
+						<li><a href="http://engineering.bryantschools.org/">Engineering</a></li>
+						<li><a href="http://robotics.bryantschools.org/">Robotics</a></li>
+						<li><a href="http://www.bryantschools.org/system/files/documents/000/000/080/BHS%20HORNET%20FOOTBALL%20SCHEDULE%202016.pdf">2016 Football</a></li>
+					</ul>
+					<br />
+				</li>
+				</li>
+			</ul>
+		</li>
+		<li><a href="http://www.bryantschools.org/staff">Staff</a></li>
+	</ul> <!-- primary-nav -->
+</nav> <!-- cd-nav -->
+
+<div id="cd-search" class="cd-search">
+	<form>
+		<input type="search" placeholder="Search...">
+	</form>
+</div>
