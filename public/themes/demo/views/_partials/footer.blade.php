f0afb97b42f734428ea04f560eb7fe612eafcfe7
illuminate3 <carsten.sapporo@gmail.com>
2016-04-11 12:52:28 -0500

    dev 16.5.1.6

--- public/themes/demo/views/_partials/footer.blade.php
+++ public/themes/demo/views/_partials/footer.blade.php
@@ -123,6 +123,32 @@
 		@endif
 
 
+		<div class="row">
+		{!! Form::open([
+			'url' => 'search/results',
+			'method' => 'POST',
+			'class' => 'form-horizontal'
+		]) !!}
+
+			<div class="form-group">
+				<label class="col-md-3 control-label">{{ Lang::choice('kotoba::general.search', 1) }}</label>
+				<div class="col-md-9">
+					<input type="text" id="search_terms" name="search_terms" placeholder="{{ trans('kotoba::general.command.enter') }} {{ Lang::choice('kotoba::general.search', 1) }}" class="form-control">
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-md-3 control-label"></label>
+				<div class="col-md-9">
+					<button type="submit" class="btn btn-success btn-block" tabindex="0">
+						{{ trans('kotoba::button.search') }}
+					</button>
+				</div>
+			</div>
+
+		{!! Form::close() !!}
+		</div> <!-- ./ row -->
+
+
 	</div>
 </div><!--./row-->
 <div class="row">
