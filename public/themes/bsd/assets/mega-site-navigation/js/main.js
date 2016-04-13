1674214a31144ae6fe7e21f994810ea487a64ce2
illuminate3 <carsten.sapporo@gmail.com>
2016-04-12 22:16:37 -0500

    dev 16.5.1.8

--- public/themes/bsd/assets/mega-site-navigation/js/main.js
+++ public/themes/bsd/assets/mega-site-navigation/js/main.js
@@ -32,7 +32,7 @@ jQuery(document).ready(function($){
 		closeNav();
 	});
 
-	//close lateral menu on mobile 
+	//close lateral menu on mobile
 	$('.cd-overlay').on('swiperight', function(){
 		if($('.cd-primary-nav').hasClass('nav-is-visible')) {
 			closeNav();
@@ -52,7 +52,7 @@ jQuery(document).ready(function($){
 	});
 
 
-	//prevent default clicking on direct children of .cd-primary-nav 
+	//prevent default clicking on direct children of .cd-primary-nav
 	$('.cd-primary-nav').children('.has-children').children('a').on('click', function(event){
 		event.preventDefault();
 	});
@@ -91,7 +91,7 @@ jQuery(document).ready(function($){
 
 	function toggleSearch(type) {
 		if(type=="close") {
-			//close serach 
+			//close serach
 			$('.cd-search').removeClass('is-visible');
 			$('.cd-search-trigger').removeClass('search-is-visible');
 			$('.cd-overlay').removeClass('search-is-visible');
@@ -107,7 +107,7 @@ jQuery(document).ready(function($){
 
 	function checkWindowWidth() {
 		//check window width (scrollbar included)
-		var e = window, 
+		var e = window,
             a = 'inner';
         if (!('innerWidth' in window )) {
             a = 'client';
@@ -122,7 +122,7 @@ jQuery(document).ready(function($){
 
 	function moveNavigation(){
 		var navigation = $('.cd-nav');
-  		var desktop = checkWindowWidth();
+		var desktop = checkWindowWidth();
         if ( desktop ) {
 			navigation.detach();
 			navigation.insertBefore('.cd-header-buttons');
