56e6aaf5ac95e3f6ebecc14bcb371765c820238a
illuminate3 <carsten.sapporo@gmail.com>
2016-06-03 15:29:17 -0500

    dev 16.7.0

--- public/themes/rakko/views/_partials/_front/footer.blade.php
+++ public/themes/rakko/views/_partials/_front/footer.blade.php
@@ -2,9 +2,11 @@
 <div class="footer-wrapper">
 <footer>
 
-	<div class="col-md-4">
-		<p class="copyright-text"><a href="/" class="logo"><img src="{{ asset('/themes/' . $activeTheme . '/assets/img/footer-logo.png') }}" alt="" /></a> &copy; 2015-2016, All rights reserved</p>
+	<div class="col-sm-4">
 
+		<a href="/" class="logo"><img src="{{ asset('/themes/' . $activeTheme . '/assets/img/footer-logo.png') }}" alt="" /></a>
+
+{{--
 		<ul class="footer-nav">
 			<li><a href="/">Contact</a></li>
 			<li><a href="/">Download</a></li>
@@ -12,19 +14,30 @@
 			<li><a href="/">Terms of Use</a></li>
 			<li><a href="/">FAQ</a></li>
 		</ul>
-	</div>
+--}}
 
-	<div class="col-md-4">
+	</div>
+	<div class="col-sm-4">
+
+		<p class="copyright-text">
+			&copy;&nbsp;2015-2016, All rights reserved
+			&nbsp;&nbsp;
+			<a href="/">info@illuminate3.com</a>
+		</p>
+
+{{--
 		<ul class="social">
 			<li><a href="/" class="fb">&nbsp;</a></li>
 			<li><a href="/" class="tw">&nbsp;</a></li>
 			<li><a href="/" class="vm">&nbsp;</a></li>
 		</ul>
+--}}
 
-		<p class="footer-email"><a href="/">info@illuminate3.com</a></p>
 	</div>
-	<div class="col-md-4">
+	<div class="col-sm-4">
+
 		@include($activeTheme . '::' . '_partials._front._extra.login')
+
 	</div>
 
 </footer>
