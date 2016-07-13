f1bff1059383e2aea262a0839b43ad1eed352103
illuminate3 <carsten.sapporo@gmail.com>
2016-07-13 15:33:26 -0500

    Dev :  16.2.9.1

--- public/themes/rakko/views/_partials/_front/_tabs/tabs.blade.php
+++ public/themes/rakko/views/_partials/_front/_tabs/tabs.blade.php
@@ -1,8 +1,29 @@
 <!-- Nav tabs -->
 <ul class="nav nav-tabs" role="tablist">
-	<li role="presentation" class="active"><a href="#news" aria-controls="news" role="tab" data-toggle="tab">{{ trans('kotoba::cms.news') }}</a></li>
-	<li role="presentation"><a href="#tweets" aria-controls="tweets" role="tab" data-toggle="tab">{{ trans('kotoba::general.twitter') }}</a></li>
-	<li role="presentation"><a href="#grams" aria-controls="tweets" role="tab" data-toggle="tab">{{ trans('kotoba::general.instagram') }}</a></li>
+	<li role="presentation" class="active">
+		<a href="#news" aria-controls="news" role="tab" data-toggle="tab">
+			<i class="fa fa-news fa-fw"></i>
+			{{ trans('kotoba::cms.news') }}
+		</a>
+	</li>
+	<li role="presentation">
+		<a href="#tweets" aria-controls="tweets" role="tab" data-toggle="tab">
+			<i class="fa fa-twitter fa-fw"></i>
+			{{ trans('kotoba::general.twitter') }}
+		</a>
+	</li>
+	<li role="presentation">
+		<a href="#grams" aria-controls="grams" role="tab" data-toggle="tab">
+			<i class="fa fa-instagram fa-fw"></i>
+			{{ trans('kotoba::general.instagram') }}
+		</a>
+	</li>
+	<li role="presentation">
+		<a href="#facebook" aria-controls="facebook" role="tab" data-toggle="tab">
+			<i class="fa fa-facebook fa-fw"></i>
+			{{ trans('kotoba::general.facebook') }}
+		</a>
+	</li>
 </ul>
 
 <!-- Tab panes -->
@@ -16,6 +37,9 @@
 	</div>
 	<div role="tabpanel" class="tab-pane" id="grams">
 		@include($activeTheme . '::' . '_partials._front._tabs.instagram')
+	</div>
+	<div role="tabpanel" class="tab-pane" id="facebook">
+		@include($activeTheme . '::' . '_partials._front._tabs.facebook')
 	</div>
 
 </div>
