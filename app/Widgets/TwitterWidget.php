7a964e5bf0e28ccc1774b19e237a558e29aafc53
illuminate3 <carsten.sapporo@gmail.com>
2016-08-05 07:50:52 -0500

    Dev :  16.3.2.5

--- app/Widgets/TwitterWidget.php
+++ app/Widgets/TwitterWidget.php
@@ -4,6 +4,60 @@ namespace App\Widgets;
 
 use Caffeinated\Widgets\Widget;
 
+use Cache;
+use Config;
+use Setting;
+use Theme;
+use Twitter;
+
+
+class TwitterWidget extends Widget
+{
+
+	public function handle()
+	{
+
+		$activeTheme = Theme::getActive();
+
+		$screen_name = Setting::get('twitter_username', Config::get('social.username'));
+		$tweet_minutes = Setting::get('tweet_minutes', Config::get('social.tweet_minutes'));
+//dd($tweet_minutes);
+
+		$tweets = Cache::get('widget_twitter', null);
+		if ($tweets == null) {
+
+			$tweets = Twitter::getUserTimeline([
+				'screen_name' => $screen_name,
+				'count' => 1,
+				false,
+				true
+				]);
+
+			Cache::put('widget_twitter', $tweets, $tweet_minutes);
+
+// 			$tweets = Cache::remember('widget_twitter', $tweet_minutes, function() {
+// 				Return $tweets;
+// 			});
+		}
+
+//dd($tweets);
+
+		return Theme::View($activeTheme . '::' . 'widgets.twitter',
+			compact(
+				'screen_name',
+				'tweets'
+		));
+
+	}
+
+
+}
+/*
+
+namespace App\Widgets;
+
+use Caffeinated\Widgets\Widget;
+
 use Config;
 use Setting;
 use Theme;
@@ -38,3 +92,5 @@ class TwitterWidget extends Widget
 
 
 }
+*/
+
